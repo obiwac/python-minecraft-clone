2ff9d781e0142a4565177065c6c1b1288ac4167e
Jukitsu <ninjax2312@gmail.com>
2021-12-30 19:11:22 +0100

    removed numpy array cuz it was slow

--- community/chunk.py
+++ community/chunk.py
@@ -22,7 +22,10 @@ class Chunk:
 			self.chunk_position[1] * CHUNK_HEIGHT,
 			self.chunk_position[2] * CHUNK_LENGTH)
 		
-		self.blocks = np.zeros((CHUNK_WIDTH, CHUNK_HEIGHT, CHUNK_LENGTH), dtype=np.uint8) # Numpy is really slow there
+		self.blocks = [[[0 for z in range(CHUNK_LENGTH)]
+							for y in range(CHUNK_HEIGHT)]
+							for x in range(CHUNK_WIDTH)]
+		# Numpy is really slow there
 
 		self.lightmap = [[[0 for z in range(CHUNK_LENGTH)]
 							for y in range(CHUNK_HEIGHT)]
