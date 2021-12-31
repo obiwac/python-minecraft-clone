c10b9a1197ae3adf831c8dbd68fa6e968157d880
Jukitsu <84381972+Jukitsu@users.noreply.github.com>
2021-12-31 01:14:45 +0100

    Remove usless options

--- community/options.py
+++ community/options.py
@@ -3,7 +3,6 @@ import pyglet.gl as gl
 
 
 TRANSLUCENT_BLENDING = 1
-ALTERNATE_BLOCKS = False # True is in WIP
 MIPMAP_TYPE = gl.GL_NEAREST # Possible filters: 
                             # Nearest filter (GL_NEAREST)
                             # Linear filter (GL_LINEAR),
@@ -11,5 +10,3 @@ MIPMAP_TYPE = gl.GL_NEAREST # Possible filters:
                             # Linear mipmap (GL_NEAREST_MIPMAP_LINEAR)
                             # Bilinear mipmap (GL_LINEAR_MIPMAP_NEAREST)
                             # Trilinear mipmap (GL_LINEAR_MIPMAP_LINEAR)
-BRIGHTNESS = 1
-VERTEX_OFFSET = True
