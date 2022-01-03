6ad4b1bf54aaca606aa728c4a1babae6dac6b2a9
obiwac <obiwac@gmail.com>
2022-01-04 00:15:07 +0100

    entity: setters for position & velocity

--- episode-12/entity.py
+++ episode-12/entity.py
@@ -7,20 +7,27 @@ class Entity:
 	def __init__(self):
 		# physical variables
 
-		self.position = [0, 80, 0]
-		self.velocity = [0, 0, 0]
 		self.acceleration = GRAVITY
+
+		self.set_position((0, 80, 0))
+		self.set_velocity((0,  0, 0))
 
 		# input variables
 
 		self.rotation = [-math.tau / 4, 0]
 		self.speed = WALKING_SPEED
 
+	def set_velocity(self, velocity):
+		self.velocity = velocity
+
+	def set_position(self, position):
+		self.position = position
+
 	def update(self, delta_time):
 		# process physics
 		
-		self.velocity = [v + a * delta_time for v, a in zip(self.velocity, self.acceleration)]
-		self.position = [p + v * delta_time for p, v in zip(self.position, self.velocity)]
+		self.set_velocity((v + a * delta_time for v, a in zip(self.velocity, self.acceleration)))
+		self.set_position((p + v * delta_time for p, v in zip(self.position, self.velocity)))
 
 		# compute collisions
 
