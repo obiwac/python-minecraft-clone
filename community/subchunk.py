907d9ae6cee9e509e6d19c323d5c96d5c5a2b13e
Jukitsu <84381972+Jukitsu@users.noreply.github.com>
2022-01-18 01:47:34 +0100

    Improved smooth lighting shading

--- community/subchunk.py
+++ community/subchunk.py
@@ -1,11 +1,24 @@
+from hashlib import sha3_512
 from util import *
 import numpy as np
 import glm
 import options
+from functools import cache, reduce
 
 SUBCHUNK_WIDTH  = 4
 SUBCHUNK_HEIGHT = 4
 SUBCHUNK_LENGTH = 4
+
+@cache
+def smooth(a, b, c, d):
+	if not a or not b or not c or not d:
+		l = (a, *filter(lambda x: x, (b, c, d)))
+		min_val = min(l)
+		a = max(a, min_val)
+		b = max(b, min_val)
+		c = max(c, min_val)
+		d = max(d, min_val)
+	return (a + b + c + d) / 4
 
 class Subchunk:
 	def __init__(self, parent, subchunk_position):
@@ -32,35 +45,41 @@ class Subchunk:
 		self.translucent_mesh = []
 		self.translucent_mesh_array = None
 
-	def get_block_light(self, block, face, pos, npos):
+	def get_raw_light(self, block, face, pos, npos):
 		if not npos:
 			light_levels = self.world.get_light(pos)
 		else:
 			light_levels = self.world.get_light(npos)
 		return [light_levels] * 4
 
-	def get_sky_light(self, block, face, pos, npos):
+	def get_raw_skylight(self, block, face, pos, npos):
 		if not npos:
 			light_levels = self.world.get_skylight(pos)
 		else:
 			light_levels = self.world.get_skylight(npos)
 		return [light_levels] * 4
 
-	def get_vertex_ao(self, light, light2, light3, corner):
-		ambient1 = glm.mix(light, light2, 1/2)
-		ambient2 = glm.mix(light, light3, 1/2)
-		ambient3 = glm.mix(light, corner, 1/2)
-		if not light and not light2:
-			return (ambient1 + ambient2) / 2
-		return (ambient1 + ambient2 + ambient3) / 3
+	def get_vertex_ao(self, s1, s2, c):
+		if s1 and s2:
+			return 0.25
+		return 1 - (s1 + s2 + c) / 4
 
-	def get_baked_face_light(self, light, light1, light2, light3,
+	def get_face_ao(self, s1, s2, s3,
+						  s4,     s5,
+						  s6, s7, s8):
+		vertex1 = self.get_vertex_ao(s2, s4, s1)
+		vertex2 = self.get_vertex_ao(s4, s7, s6)
+		vertex3 = self.get_vertex_ao(s5, s7, s8)
+		vertex4 = self.get_vertex_ao(s2, s5, s3)
+		return (vertex1, vertex2, vertex3, vertex4)
+
+	def get_smooth_face_light(self, light, light1, light2, light3,
 										  light4,		  light5,
 										  light6, light7, light8):
-		vertex1 = self.get_vertex_ao(light, light2, light4, light1)
-		vertex2 = self.get_vertex_ao(light, light4, light7, light6)
-		vertex3 = self.get_vertex_ao(light, light5, light7, light8)
-		vertex4 = self.get_vertex_ao(light, light2, light5, light3)
+		vertex1 = smooth(light, light2, light4, light1)
+		vertex2 = smooth(light, light4, light7, light6)
+		vertex3 = smooth(light, light5, light7, light8)
+		vertex4 = smooth(light, light2, light5, light3)
 		return (vertex1, vertex2, vertex3, vertex4)
 
 	def get_neighbour_voxels(self, npos, face):
@@ -105,7 +124,7 @@ class Subchunk:
 
 
 
-	def get_baked_light(self, block, face, pos, npos):
+	def get_light_smooth(self, block, face, pos, npos):
 		if not npos or block in self.world.light_blocks:
 			return [self.world.get_light(pos)] * 4
 
@@ -113,9 +132,9 @@ class Subchunk:
 
 		nlights = (self.world.get_light(neighbour_pos) for neighbour_pos in neighbours)
 
-		return self.get_baked_face_light(self.world.get_light(npos), *nlights)
+		return self.get_smooth_face_light(self.world.get_light(npos), *nlights)
 
-	def get_baked_skylight(self, block, face, pos, npos):
+	def get_skylight_smooth(self, block, face, pos, npos):
 		if not npos or block in self.world.light_blocks:
 			return [self.world.get_skylight(pos)] * 4
 
@@ -123,18 +142,32 @@ class Subchunk:
 		
 		nlights = (self.world.get_skylight(neighbour_pos) for neighbour_pos in neighbours)
 
-		return self.get_baked_face_light(self.world.get_skylight(npos), *nlights)
+		return self.get_smooth_face_light(self.world.get_skylight(npos), *nlights)
 
-	get_light = get_baked_light if options.SMOOTH_LIGHTING else get_block_light
-	get_skylight = get_baked_skylight if options.SMOOTH_LIGHTING else get_sky_light
+	def get_ambient(self, block, block_type, face, pos, npos):
+		raw_shading = block_type.shading_values[face]
+		if not block_type.is_cube or block in self.world.light_blocks:
+			return raw_shading
+
+		neighbours = self.get_neighbour_voxels(npos, face)
+		
+		neighbour_opacity = (self.world.is_opaque_block(neighbour_pos) for neighbour_pos in neighbours)
+
+		face_ao = self.get_face_ao(*neighbour_opacity)
+		
+		return [a * b for a, b in zip(face_ao, raw_shading)]
+
+	get_shading = get_ambient if options.SMOOTH_LIGHTING else lambda self, b, block_type, face, pos, npos: block_type.shading_values[face]
+	get_light = get_light_smooth if options.SMOOTH_LIGHTING else get_raw_light
+	get_skylight = get_skylight_smooth if options.SMOOTH_LIGHTING else get_raw_skylight
 
 	def add_face(self, face, pos, lpos, block, block_type, npos=None):
 		lx, ly, lz = lpos
 		vertex_positions = block_type.vertex_positions[face]
 		tex_index = block_type.tex_indices[face]
-		shading_values = block_type.shading_values[face]
-		baked_lights = self.get_light(block, face, pos, npos)
-		baked_skylights = self.get_skylight(block, face, pos, npos)
+		shading = self.get_shading(block, block_type, face, pos, npos)
+		lights = self.get_light(block, face, pos, npos)
+		skylights = self.get_skylight(block, face, pos, npos)
 
 		if block_type.model.translucent:
 			mesh = self.translucent_mesh
@@ -146,9 +179,9 @@ class Subchunk:
 					 vertex_positions[i * 3 + 1] + ly, 
 					 vertex_positions[i * 3 + 2] + lz,
 					 tex_index * 4 + i,
-					 shading_values[i],
-					 baked_lights[i],
-					 baked_skylights[i]]
+					 shading[i],
+					 lights[i],
+					 skylights[i]]
 					 
 
 	def can_render_face(self, block_type, block_number, position):
