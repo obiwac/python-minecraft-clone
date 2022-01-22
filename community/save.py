1082ad802b77ead95509c77273ea9be687399dbf
obiwac <obiwac@gmail.com>
2022-01-22 01:00:19 +0100

    save: added stuff

--- community/save.py
+++ community/save.py
@@ -84,8 +84,8 @@ class Save:
 		# 	for y in range(-15, 1):
 		# 		self.load_chunk((x, y))
 
-		for x in range(-1, 1):
-			for y in range(-1, 1):
+		for x in range(-4, 4):
+			for y in range(-4, 4):
 				self.load_chunk((x, 0, y))
 			
 		for chunk_position, unlit_chunk in self.world.chunks.items():
