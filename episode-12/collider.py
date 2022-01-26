8b75bc2ec465e0db5d3403718cca5fb4a0d8bd44
obiwac <obiwac@gmail.com>
2022-01-26 18:04:54 +0100

    collider: made anonymous function's name more explicit

--- episode-12/collider.py
+++ episode-12/collider.py
@@ -31,16 +31,16 @@ class Collider:
 
 		vx, vy, vz = velocity
 
-		div = lambda x, y: x / y if y else float('-' * (x > 0) + "inf")
+		time = lambda x, y: x / y if y else float('-' * (x > 0) + "inf")
 
-		x_entry = div(collider.x1 - self.x2 if vx > 0 else collider.x2 - self.x1, vx)
-		x_exit  = div(collider.x2 - self.x1 if vx > 0 else collider.x1 - self.x2, vx)
+		x_entry = time(collider.x1 - self.x2 if vx > 0 else collider.x2 - self.x1, vx)
+		x_exit  = time(collider.x2 - self.x1 if vx > 0 else collider.x1 - self.x2, vx)
 
-		y_entry = div(collider.y1 - self.y2 if vy > 0 else collider.y2 - self.y1, vy)
-		y_exit  = div(collider.y2 - self.y1 if vy > 0 else collider.y1 - self.y2, vy)
+		y_entry = time(collider.y1 - self.y2 if vy > 0 else collider.y2 - self.y1, vy)
+		y_exit  = time(collider.y2 - self.y1 if vy > 0 else collider.y1 - self.y2, vy)
 
-		z_entry = div(collider.z1 - self.z2 if vz > 0 else collider.z2 - self.z1, vz)
-		z_exit  = div(collider.z2 - self.z1 if vz > 0 else collider.z1 - self.z2, vz)
+		z_entry = time(collider.z1 - self.z2 if vz > 0 else collider.z2 - self.z1, vz)
+		z_exit  = time(collider.z2 - self.z1 if vz > 0 else collider.z1 - self.z2, vz)
 
 		# make sure we actually got a collision
 
