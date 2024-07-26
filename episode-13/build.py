54fa5ba1040b03c8bb9a431fbe93176cfe532b50
Aymeric Wibo <obiwac@gmail.com>
2024-07-27 00:03:14 +0200

    ep13: Build extensions inplace

--- episode-13/build.py
+++ episode-13/build.py
@@ -4,27 +4,24 @@ import Cython.Compiler.Options
 
 Cython.Compiler.Options.cimport_from_pyx = True  # needed?
 
-ext_modules = cythonize(
-	[
-		"src/chunk/__init__.pyx",
-		"src/chunk/chunk.pyx",
-		"src/chunk/subchunk.pyx",
-	],
-	compiler_directives={
-		"language_level": 3,
-		"profile": True,
-	},
-	annotate=True,
-)
-
-
-class ExtBuilder(build_ext):
-	def run(self):
-		build_ext.run(self)
-
+class BuildExt(build_ext):
 	def build_extension(self, ext):
-		build_ext.build_extension(self, ext)
+		self.inplace = True # Important or the LSP won't have access to the compiled files.
+		super().build_extension(ext)
 
 
 def build(setup_kwargs):
-	setup_kwargs.update({"ext_modules": ext_modules, "cmdclass": {"build_ext": ExtBuilder}})
+	ext_modules = cythonize(
+		[
+			"src/chunk/__init__.pyx",
+			"src/chunk/chunk.pyx",
+			"src/chunk/subchunk.pyx",
+		],
+		compiler_directives={
+			"language_level": 3,
+			"profile": True,
+		},
+		annotate=True,
+	)
+
+	setup_kwargs.update({"ext_modules": ext_modules, "cmdclass": {"build_ext": BuildExt}})
